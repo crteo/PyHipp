8eaaebf424d19acdee357d0040f8c60230c8318d
AWS ParallelCluster user <ec2-user@ip-10-0-8-239.ap-southeast-1.compute.internal>
2025-09-14 04:27:07 +0000

    latest files from lab 5

--- rplparallel-slurm.sh
+++ rplparallel-slurm.sh
@@ -2,7 +2,7 @@
 
 # Submit this script with: sbatch <this-filename>
 
-#SBATCH --time=4:00:00   # walltime
+#SBATCH --time=24:00:00   # walltime
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH -J "rplpl"   # job name
@@ -27,5 +27,5 @@ pyh.raycast(1); \
 print(time.localtime()); \
 print(time.time()-t0);"
 
-aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:282927509024:awsnotify --message "RPLParallelJobDone"
+#aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:282927509024:awsnotify --message "RPLParallelJobDone"
 
